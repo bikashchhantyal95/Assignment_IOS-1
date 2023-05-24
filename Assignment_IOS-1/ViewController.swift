65800dc39c31b9e628bddad7a46c197a687be4ef
Bikash Chhantyal <bikashchhantyal2@gmail.com>
2023-05-24 16:35:41 -0400

    Initial Commit

---
+++ Assignment_IOS-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment_IOS-1
+//
+//  Created by Bikash Chhantyal on 2023-05-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
