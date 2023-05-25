96fadb71d532e9097de2123b8330dfe577987fbb
Bikash Chhantyal <bikashchhantyal2@gmail.com>
2023-05-24 23:00:47 -0400

    Added funtionality to buttons

--- Assignment_IOS-1/ViewController.swift
+++ Assignment_IOS-1/ViewController.swift
@@ -20,7 +20,25 @@ class ViewController: UIViewController {
         let header = "x-apisports-key"
         let authToken = "d69036aadf632739eccdb08c7821cb77"
         Task{
+            print("Sports data API:-")
             print(await API_Helper.fetchData(urlString: url, authToken: authToken, header: header)!)
         }
     }
+    
+    @IBAction func getAnimeTitleAndQuotes(_ sender: Any) {
+        let url = "https://animechan.vercel.app/api/quotes"
+        Task{
+            print("Anime Title and Quotes Data Api:-")
+            print(await API_Helper.fetchData(urlString: url)!)
+        }
+    }
+    
+    @IBAction func getDigimonCharacters(_ sender: Any) {
+        let url = "https://digimon-api.vercel.app/api/digimon"
+        Task{
+            print("Digimon Characters Data Api:-")
+            print(await API_Helper.fetchData(urlString: url)!)
+        }
+    }
+   
 }
